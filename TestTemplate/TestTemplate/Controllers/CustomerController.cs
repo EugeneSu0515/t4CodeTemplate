1c4dab4cc007b07d171d4d7e2457b37a186b203d
EugeneSu0515 <beckhamnaing72@gmail.com>
2014-07-08 16:55:26 +0800

    ver 1.00

---
+++ TestTemplate/TestTemplate/Controllers/CustomerController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+
+using TestTemplate.Models;
+
+namespace TestTemplate.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private AdventureWorksLT2008_DataEntities db = new AdventureWorksLT2008_DataEntities();
+
+        //
+        // GET: /Customer/
+
+        public ActionResult Index(int pageID=1)
+        {
+            return View(db.Customer.ToList().ToPagedList(pageID, pageSize: 10));
+        }
+
+        //
+        // GET: /Customer/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Customer customer = db.Customer.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        //
+        // GET: /Customer/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Customer/Create
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customer.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        //
+        // GET: /Customer/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Customer customer = db.Customer.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        //
+        // POST: /Customer/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        //
+        // GET: /Customer/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Customer customer = db.Customer.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        //
+        // POST: /Customer/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customer.Find(id);
+            db.Customer.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
